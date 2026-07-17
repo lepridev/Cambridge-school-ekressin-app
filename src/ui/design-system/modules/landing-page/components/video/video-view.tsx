8767b3bcc2a743cf8c8e9ac0fa3eb7bc3629ddba
Armand Lepri <lepri@MacBook-Pro-de-Armand.local>
2026-07-17 09:30:36 +0000

    Remplacement de cours vance par cours d'emglais

--- src/ui/design-system/modules/landing-page/components/video/video-view.tsx
+++ src/ui/design-system/modules/landing-page/components/video/video-view.tsx
@@ -9,7 +9,7 @@ export const VideoView = () => {
       <Container className="md:py-20 p-10">
         <div className="flex flex-col justify-center text-center space-y-4">
           <Typograpy variant="h2" component="h2">
-            COURS DE VACANCES EN ANGLAIS
+            COURS D&apos;ANGLAIS TOUT AU LONG DE L&apos;ANNÉE SCOLAIRE
           </Typograpy>
           <Typograpy
             variant="lead"
@@ -17,16 +17,19 @@ export const VideoView = () => {
             weight="medium"
             className="text-gray-700"
           >
-            Apprends anglais de manière interactive
+            Apprends l&apos;anglais de manière interactive pendant toute l&apos;année
           </Typograpy>
           <Typograpy variant="caption3" component="p" theme="gray">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-            esse dolorem possimus ut architecto error placeat iure eum?
-            Repellendus placeat itaque sequi ipsa delectus aut praesentium
-            nulla, nisi alias dignissimos!
+            Nos cours d&apos;anglais complets sont conçus pour aider les élèves
+            à développer leurs compétences linguistiques durant l&apos;année scolaire.
+            Grâce à des leçons interactives, des activités engageantes et un
+            accompagnement personnalisé, nous guidons les apprenants vers la
+            maîtrise de la langue. Que vous partiez de zéro ou que vous souhaitiez
+            perfectionner vos compétences, nos programmes s&apos;adaptent à tous les
+            niveaux et à tous les rythmes d&apos;apprentissage.
           </Typograpy>
         </div>
-        <div className="md:w-[100%] w-[100%] h-[200px] relative md:h-[626px] rounded md:mt-10">
+        <div className="md:w-[100%] w-[100%] h-[200px] relative md:h-[626px] md:mt-10">
           {/* <div className="bg-gray absolute w-full h-full  z-10 opacity-0 hover:opacity-75 animate rounded text-white flex flex-col justify-center items-center">
             <RiPlayCircleFill
               size={50}
@@ -38,10 +41,10 @@ export const VideoView = () => {
           </div> */}
 
           <Image
-            src="/assets/images/vacances.jpeg"
-            alt="suivre le cours pour mieux appréhender le concepte"
+            src="/assets/images/heroPhoto1.jpeg"
+            alt="Cours d&apos;anglais pour l&apos;année scolaire"
             fill
-            className="rounded object-contain"
+            className="rounded-xl object-cover"
           />
         </div>
       </Container>
